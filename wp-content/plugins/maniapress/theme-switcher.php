0e1a4d09b70ac1d5c005fe43bb1801a85f492287
Maxime Raoust <gou1@users.noreply.github.com>
2011-10-21 14:32:17 +0000

    ManiaPress Theme Switcher:  automatically selects the ManiaPress theme when accessing a page in the Manialink browser of Maniaplanet, based on the User Agent.

---
+++ wp-content/plugins/maniapress/theme-switcher.php
@@ -0,0 +1,56 @@
+<?php
+/*
+  Plugin Name: ManiaPress Theme Switcher
+  Plugin URI: http://code.google.com/p/maniapress/
+  Description: This plugin is part of ManiaPress: a suite to display your WordPress install directly in Maniaplanet. This plugin automatically selects the ManiaPress theme when accessing a page in the Manialink browser of Maniaplanet, based on the User Agent.
+  Version: beta
+  Author: Nadeo
+  Author URI: http://www.nadeo.com
+  License: LGPL v3
+ */
+
+require_once __DIR__.'/core.php';
+
+function maniapress_theme_switcher_activate()
+{
+	require_once ABSPATH.'wp-admin/includes/plugin.php';
+	
+	if(!is_plugin_active('maniapress/core.php'))
+	{
+		$message = 'It seems the ManiaPress Core plugin is not activated. Please <strong>activate the ManiaPress Core plugin</strong> before using other components of ManiaPress';
+		trigger_error($message, E_USER_ERROR);
+	}
+}
+
+register_activation_hook(__FILE__, 'maniapress_theme_switcher_activate');
+
+function maniapress_check_user_agent()
+{
+	//return true;
+	return ManiaLib\Application\Filters\UserAgentCheck::isManiaplanet();
+}
+
+function maniapress_installed()
+{
+	return is_dir(MANIAPRESS_THEME_PATH);
+}
+
+function maniapress_template($theme)
+{
+	if(maniapress_installed())
+	{
+		return apply_filters('maniapress_template', MANIAPRESS_THEME);
+	}
+	else
+	{
+		return $theme;
+	}
+}
+
+if(maniapress_check_user_agent())
+{
+	add_filter('template', 'maniapress_template');
+	add_filter('option_template', 'maniapress_template');
+	add_filter('option_stylesheet', 'maniapress_template');
+}
+?>
