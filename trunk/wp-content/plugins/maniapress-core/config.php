43c43bc136e31d72aa2ffd9caf183193f82142f1
maximeraoust <maximeraoust@e31109a7-68bc-a5bb-e34d-e94333b26e10>
2011-10-24 16:04:36 +0000

    folder structure...

--- trunk/wp-content/plugins/maniapress-core/config.php
+++ trunk/wp-content/plugins/maniapress-core/config.php
@@ -1,8 +1,18 @@
 <?php
 /**
- * @copyright   Copyright (c) 2009-2011 NADEO (http://www.nadeo.com)
+ * ManiaPress: a suite to display your WordPress install directly in Maniaplanet.
+ * 
+ * @see         http://code.google.com/p/maniapress/
+ * @copyright   Copyright (c) 2011-2012 NADEO (http://www.nadeo.com)
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
  * @version     $Revision$:
  * @author      $Author$:
  * @date        $Date$:
  */
+
+/**
+ * Config for ManiaPress. Eventually it will be handled by an admin page, in the 
+ * meantime we rely on a good old config file...
+ */
+
 ?>
